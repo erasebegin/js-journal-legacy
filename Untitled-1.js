5b37eeb6518c7292e673630c79422911e09314bb
erasebegin <chris.james.haupt@gmail.com>
2019-06-10 20:52:45 +0800

    adding several new articles

---
+++ Untitled-1.js
@@ -0,0 +1,13 @@
+function titleCase(str) {
+  let wordArr = str.split(' ');
+  let capitalArr = [];
+  let fullArr= [];
+  for (let i = 0; i < 9; i++){
+      fullArr.push(wordArr[i].split(''));
+     for (let j = 0; j < wordArr[i].length; j++){
+         capitalArr.push('g')
+        }console.log(fullArr)
+    } 
+  }
+
+  titleCase("I'm a little tea pot");
